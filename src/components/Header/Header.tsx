0f2b8685dcd12115927ed62cf0bb8284b479fc67
Artem <bezkorovainy2006@gmail.com>
2024-09-02 11:46:21 +0200

    added header component, added home component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+
+
+const Header = () => {
+    return (
+    <header className="header">Rest Countries APP!!</header>
+    );
+}
+
+export default Header;
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,10 @@
+export interface Country {
+    name: string;
+    capital: string;
+    region: string;
+    population: string;
+    flags: {
+        png: string;
+        svg: string;
+    };
+}
